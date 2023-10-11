dfdbb608714a98eaeeea76a7024fdc0b328b8b96
miqqra <mikhail.krasnikov.04@gmail.com>
2023-10-11 23:31:01 +0700

    Initial commit

---
+++ src/main/java/mydudesgeo/MyDudesGeoApplication.java
@@ -0,0 +1,13 @@
+package mydudesgeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDudesGeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDudesGeoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mydudesgeo/MyDudesGeoApplicationTests.java
@@ -0,0 +1,13 @@
+package mydudesgeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDudesGeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
