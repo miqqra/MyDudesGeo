3535ce9f85c205f5da9a825434404acbe1d904e4
miqqra <mikhail.krasnikov.04@gmail.com>
2024-05-22 22:26:37 +0700

    fix friends requests functionality

--- src/main/java/mydudesgeo/controller/user/FriendRequestController.java
+++ src/main/java/mydudesgeo/controller/user/FriendRequestController.java
@@ -2,7 +2,11 @@ package mydudesgeo.controller.user;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import mydudesgeo.dto.friend.FriendRequestDto;
+import mydudesgeo.service.FriendRequestService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,24 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/mydudes/geo/friends/requests")
 public class FriendRequestController {
 
+    private final FriendRequestService service;
+
     @Operation(summary = "Отправить запрос дружбы")
     @PostMapping("/send/{to}")
     public void sendRequest(
             @Parameter(description = "Пользователь, которому отправляется запрос") @PathVariable String to) {
-        //todo
+        service.sendRequest(to);
     }
 
     @Operation(summary = "Принять запрос дружбы")
-    @PostMapping("/accept/{to}")
+    @PostMapping("/accept/{from}")
     public void acceptRequest(
-            @Parameter(description = "Пользователь, которому отправляется запрос") @PathVariable String to) {
-        //todo
+            @Parameter(description = "Пользователь, которому отправляется запрос") @PathVariable String from) {
+        service.acceptRequest(from);
     }
 
     @Operation(summary = "Отклонить запрос дружбы")
-    @PostMapping("/reject/{to}")
+    @PostMapping("/reject/{from}")
     public void rejectRequest(
-            @Parameter(description = "Пользователь, которому отправляется запрос") @PathVariable String to) {
-        //todo
+            @Parameter(description = "Пользователь, которому отправляется запрос") @PathVariable String from) {
+        service.rejectRequest(from);
+    }
+
+    @Operation(summary = "Посмотреть запросы дружбы")
+    @GetMapping
+    public List<FriendRequestDto> getRequests() {
+        return service.getRequests();
     }
 }
--- src/main/java/mydudesgeo/dataservice/FriendRequestDataService.java
+++ src/main/java/mydudesgeo/dataservice/FriendRequestDataService.java
@@ -1,13 +1,14 @@
 package mydudesgeo.dataservice;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import mydudesgeo.data.FriendRequestStatus;
 import mydudesgeo.mapper.FriendRequestMapper;
 import mydudesgeo.model.FriendRequestModel;
 import mydudesgeo.repository.FriendRequestRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +17,15 @@ public class FriendRequestDataService {
     private final FriendRequestRepository repository;
 
     private final FriendRequestMapper mapper;
+
+    public List<FriendRequestModel> getRequests(String currentUser) {
+        return Optional.of(currentUser)
+                .map(v -> repository.getFriendRequestByRequestTo(v, FriendRequestStatus.NOT_VIEWED))
+                .stream()
+                .flatMap(Collection::stream)
+                .map(mapper::toModel)
+                .toList();
+    }
 
     public FriendRequestModel sendRequest(FriendRequestModel model) {
         return Optional.of(model)
